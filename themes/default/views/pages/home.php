71a60b3d4dcfbcab656e7db3c431721b4bd5e0aa
DaWebzah <dawebzah@gmail.com>
2016-11-26 16:30:31 -0200

    latest

--- themes/default/views/pages/home.php
+++ themes/default/views/pages/home.php
@@ -1,42 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
-<?if(core::config('advertisement.ads_in_home') != 3):?>
-    <div class="well">
-        <h3>
-            <?if(core::config('advertisement.ads_in_home') == 0):?>
-                <?=_e('Latest Ads')?>
-            <?elseif(core::config('advertisement.ads_in_home') == 1 OR core::config('advertisement.ads_in_home') == 4):?>
-                <?=_e('Featured Ads')?>
-            <?elseif(core::config('advertisement.ads_in_home') == 2):?>
-                <?=_e('Popular Ads last month')?>
-            <?endif?>
-            <?if ($user_location) :?>
-                <small><?=$user_location->name?></small>
-            <?endif?>
-        </h3>
-        <div class="row">
-            <?$i=0; foreach($ads as $ad):?>
-                <div class="col-md-3">
-                    <div class="thumbnail latest_ads">
-                        <a href="<?=Route::url('ad', array('category'=>$ad->category->seoname,'seotitle'=>$ad->seotitle))?>"  class="min-h">
-                            <?if($ad->get_first_image()!== NULL):?>
-                                <?=HTML::picture($ad->get_first_image(), ['w' => 132, 'h' => 132], ['992px' => ['w' => '132', 'h' => '132'], '320px' => ['w' => '648', 'h' => '648']], ['alt' => HTML::chars($ad->title)])?>
-                            <?elseif( ($icon_src = $ad->category->get_icon()) !== FALSE):?>
-                                <?=HTML::picture($icon_src, ['w' => 132, 'h' => 132], ['992px' => ['w' => '132', 'h' => '132'], '320px' => ['w' => '648', 'h' => '648']], ['alt' => HTML::chars($ad->title)])?>
-                            <?else:?>
-                                <img data-src="holder.js/179x179?<?=str_replace('+', ' ', http_build_query(array('text' => $ad->category->name, 'size' => 14, 'auto' => 'yes')))?>" alt="<?=HTML::chars($ad->title)?>">
-                            <?endif?>
-                        </a>
-                        <div class="caption">
-                            <h5><a href="<?=Route::url('ad', array('controller'=>'ad','category'=>$ad->category->seoname,'seotitle'=>$ad->seotitle))?>"><?=$ad->title?></a></h5>
-                            <p ><?=Text::limit_chars(Text::removebbcode($ad->description), 30, NULL, TRUE)?></p>
-                        </div>
-                    </div>
-                </div>
-            <?endforeach?>
-        </div>
-    </div>
-<?endif?>
-<div class='well'>
+
+
     <h3>
         <?=_e("Categories")?>
         <?if ($user_location) :?>
@@ -72,7 +36,45 @@
             <?endif?>
         <?endforeach?>
     </div>
-</div>
+
+<?if(core::config('advertisement.ads_in_home') != 3):?>
+
+        <h3>
+            <?if(core::config('advertisement.ads_in_home') == 0):?>
+                <?=_e('Latest Ads')?>
+            <?elseif(core::config('advertisement.ads_in_home') == 1 OR core::config('advertisement.ads_in_home') == 4):?>
+                <?=_e('Featured Ads')?>
+            <?elseif(core::config('advertisement.ads_in_home') == 2):?>
+                <?=_e('Popular Ads last month')?>
+            <?endif?>
+            <?if ($user_location) :?>
+                <small><?=$user_location->name?></small>
+            <?endif?>
+        </h3>
+        <div class="row">
+            <?$i=0; foreach($ads as $ad):?>
+                <div class="col-md-3">
+                    <div class="thumbnail latest_ads">
+                        <a href="<?=Route::url('ad', array('category'=>$ad->category->seoname,'seotitle'=>$ad->seotitle))?>"  class="min-h">
+                            <?if($ad->get_first_image()!== NULL):?>
+                                <?=HTML::picture($ad->get_first_image(), ['w' => 132, 'h' => 132], ['992px' => ['w' => '132', 'h' => '132'], '320px' => ['w' => '648', 'h' => '648']], ['alt' => HTML::chars($ad->title)])?>
+                            <?elseif( ($icon_src = $ad->category->get_icon()) !== FALSE):?>
+                                <?=HTML::picture($icon_src, ['w' => 132, 'h' => 132], ['992px' => ['w' => '132', 'h' => '132'], '320px' => ['w' => '648', 'h' => '648']], ['alt' => HTML::chars($ad->title)])?>
+                            <?else:?>
+                                <img data-src="holder.js/179x179?<?=str_replace('+', ' ', http_build_query(array('text' => $ad->category->name, 'size' => 14, 'auto' => 'yes')))?>" alt="<?=HTML::chars($ad->title)?>">
+                            <?endif?>
+                        </a>
+                        <div class="caption">
+                            <h5><a href="<?=Route::url('ad', array('controller'=>'ad','category'=>$ad->category->seoname,'seotitle'=>$ad->seotitle))?>"><?=$ad->title?></a></h5>
+                            <p ><?=Text::limit_chars(Text::removebbcode($ad->description), 30, NULL, TRUE)?></p>
+                        </div>
+                    </div>
+                </div>
+            <?endforeach?>
+        </div>
+
+<?endif?>
+
 <?if(core::config('general.auto_locate') AND ! Cookie::get('user_location') AND Core::is_HTTPS()):?>
     <input type="hidden" name="auto_locate" value="<?=core::config('general.auto_locate')?>">
     <?if(count($auto_locats) > 0):?>
